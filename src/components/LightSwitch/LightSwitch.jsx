5e5bfcd22a70cd876045b5617247a316bd404c53
Niem Nguyen <luuniem87@gmail.com>
2019-04-30 02:51:42 -0700

    add switches on and off to app

--- src/components/LightSwitch/LightSwitch.jsx
+++ src/components/LightSwitch/LightSwitch.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 
 const LightSwitch = props => {
-  const { lightSwitch } = props;
+  const { lightSwitch, lightSwitchOff } = props;
   return (
-    <Button variant="contained" color="primary" onClick={lightSwitch}>
-      Light Switch
-    </Button>
+    <div className="flex-box">
+      <Button variant="contained" color="default" onClick={lightSwitch}>
+        Light On
+      </Button>
+      <Button variant="contained" color="primary" onClick={lightSwitchOff}>
+        Light Off
+      </Button>
+    </div>
   );
 };
 
--- src/containers/MainScreen/MainScreen.jsx
+++ src/containers/MainScreen/MainScreen.jsx
@@ -2,15 +2,54 @@ import React, { PureComponent } from "react";
 import LightSwitch from "./../../components/LightSwitch/LightSwitch";
 import Switches from "./../Switches/Switches";
 
+const username = "efhX0bOeYSyCShCbb7maUfdZd-80624DviBfbXZh";
+const api_link = "https://192.168.1.106/api/";
+
 class MainScreen extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      lightData: []
+    };
   }
+
+  async componentDidMount() {
+    const api_call = await fetch(`${api_link}${username}/lights/4/`);
+    const api_data = await api_call.json();
+    this.setState({ lightData: api_data });
+    console.log(this.state.lightData);
+    // console.log(this.state.lightData);
+  }
+
+  switchLightHandler = async lightState => {
+    const url = `${api_link}${username}/lights/4/state`;
+    fetch(url, {
+      method: "PUT",
+      body: JSON.stringify({
+        on: true
+      })
+    });
+  };
+  switchOffHandler = async lightState => {
+    const url = `${api_link}${username}/lights/4/state`;
+    fetch(url, {
+      method: "PUT",
+      body: JSON.stringify({
+        on: false
+      })
+    });
+  };
+
   render() {
+    const { switchLightHandler, switchOffHandler } = this;
+
     return (
       <div>
-        <LightSwitch className="primary-color" />
+        <LightSwitch
+          className="primary-color"
+          lightSwitch={switchLightHandler}
+          lightSwitchOff={switchOffHandler}
+        />
       </div>
     );
   }
