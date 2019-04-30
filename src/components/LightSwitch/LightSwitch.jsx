5abcfe6c434a57cf71296482c819ccb44be0715b
Niem Nguyen <luuniem87@gmail.com>
2019-04-30 03:22:30 -0700

    add styles

--- src/components/LightSwitch/LightSwitch.jsx
+++ src/components/LightSwitch/LightSwitch.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+import "./LightSwitch.scss";
 
 const LightSwitch = props => {
   const { lightSwitch, lightSwitchOff } = props;
